45174ecc1c413d7a6c0e324a99ee4f9d35dda448
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-05-26 20:23:45 +0000

    Preparing to the first release.

--- assets/Code/Algorythms/Merge Sort/merge-sort.hs
+++ assets/Code/Algorythms/Merge Sort/merge-sort.hs
@@ -1,26 +1,69 @@
-main = (print . mergesort) ["3"]
+-- MERGE SORT --
 
-mergesort :: [String] -> [String]
-mergesort = mergesort' . map wrap
+main = let array = [ "D", "C", "B", "A" ]
+           result = sort array
+           
+        in print result
 
+{-
+    Break down an array into
+    one elements arrays with
+    map wrap function.
+    [ "D", "C", "B", "A" ] =>
+    [ ["D"], ["C"], ["B"], ["A"] ]
+
+    later will be merged by
+    mergesort' function like this
+    [ ["D"], ["C"], ["B"], ["A"] ] =>
+    [ ["C", "D"], ["A", "B"] ] =>
+    [ ["A, "B", "C", "D"] ] =>
+    ["A, "B", "C", "D"]
+-}
+sort :: [String] -> [String]
+sort = mergesort' . map wrap
+
+
+-- Merges arrays up until there is only one to be returned.
 mergesort' :: [[String]] -> [String]
 mergesort' [] = []
-mergesort' [xs] = xs
+
+-- Single array means that all sub-arrays have been sorted:
+mergesort' [single] = single
+
+-- Sort sub-arrays if two or more and repeat until there is only one:
 mergesort' xss = mergesort' (merge_pairs xss)
 
+
+{- 
+    Iterating through array pairs, 
+    making one array from each two arrays.
+    ["A", "D"] ["B", "C"] ["E"] =>
+    ["A", "B", "C", "D"] ["E"]
+-}
 merge_pairs :: [[String]] -> [[String]]
 merge_pairs [] = []
 merge_pairs [xs] = [xs]
 merge_pairs (xs:ys:xss) = merge xs ys : merge_pairs xss
 
+
+{- 
+    Merges two sorted arrays into one sorted.
+    ["B"] ["A"] => ["A", "B"]
+-}
 merge :: [String] -> [String] -> [String]
+
+-- Check empty arrays:
 merge [] ys = ys
 merge xs [] = xs
+
+-- Merge by comparing first elements of arrays:
 merge (x:xs) (y:ys)
  =
-    if x > y
-        then (y : merge (x:xs)  ys)
-        else (x : merge  xs    (y:ys))
+    if x < y
+        then (x : merge  xs    (y:ys))
+        else (y : merge (x:xs)  ys)
 
+
+-- Wrap single string into array.
 wrap :: String -> [String]
 wrap x = [x]
