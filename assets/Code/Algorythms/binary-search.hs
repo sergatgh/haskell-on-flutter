e9d5ffc7b4e3520f069876b6581d26e655cbfad0
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-05-26 18:02:06 +0000

    Added comments and modified variables in binary search.

--- assets/Code/Algorythms/binary-search.hs
+++ assets/Code/Algorythms/binary-search.hs
@@ -1,12 +1,31 @@
-main =  let arr = [1,2,3,4,5,6,7,8,9]
-            search = 3
-        in print (binsearch arr search 0 (length arr))
+-- BINARY SEARCH --
 
-binsearch :: [Int] -> Int -> Int -> Int -> Int
-binsearch xs value low high
-   | high < low       = -1
-   | xs!!mid > value  = binsearch xs value low (mid-1)
-   | xs!!mid < value  = binsearch xs value (mid+1) high
-   | otherwise        = mid
+-- Find member 3 in array
+main = let array = [1,2,3,4,5,6,7,8,9]
+           member = 3
+           from = 0
+           to = (length array)
+
+           index = search member array from to
+
+        -- Output result '2'
+        in print (index)
+
+
+-- Accepts 'value' and 'array' and a range of search in array.
+search :: Int -> [Int] -> Int -> Int -> Int
+search value array low high
+   
+   -- Search can not be executed:
+   | high < low          = -1
+   | high < 0            = -1
+   | length array < low  = -1
+
+   -- Check the middle value (mid will be calculated later):
+   | array!!mid > value  = search value array low (mid-1)
+   | array!!mid < value  = search value array (mid+1) high
+   | otherwise           = mid
+   
+   -- Calculate middle value:
    where
    mid = low + ((high - low) `div` 2)
