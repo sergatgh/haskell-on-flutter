6adcc7c057a0d9e6e68bf52c79d35431c3c57417
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-05-27 21:36:44 +0000

    Added information to count lines functionality.

--- assets/Code/Beginning/count-file-lines.hs
+++ assets/Code/Beginning/count-file-lines.hs
@@ -1,10 +1,22 @@
-import System.Environment  
-import System.IO  
-import System.Directory  
-  
-main = do (fileName:_) <- getArgs  
-          fileExists <- doesFileExist fileName  
-          if fileExists  
-              then do contents <- readFile fileName  
-                      putStrLn \$ "The file has " ++ show (length (lines contents)) ++ " lines!"  
-              else do putStrLn "The file doesn't exist!"
+-- COUNT LINES --
+
+-- To use 'getArgs'
+import System.Environment
+
+-- To use 'doesFileExist':
+import System.Directory
+
+main = do
+        -- Read CLI arguments into array (x:xs) where first value should be file name:
+        (fileName:_) <- getArgs
+
+        -- Check for file existence:
+        fileExists <- doesFileExist fileName
+        
+        if fileExists
+        
+        -- Basic logic to read file, read lines, count lines and show the result:
+        then do contents <- readFile fileName
+                putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
+        
+        else do putStrLn "The file doesn't exist!"
--- assets/Code/Beginning/hello-world.hs
+++ assets/Code/Beginning/hello-world.hs
@@ -1,1 +1,3 @@
+-- HELLO WORLD --
+
 main = putStrLn "Hello, world!"
