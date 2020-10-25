79f8ef0816c7b8932f79fd1b479c163ad322f95a
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-10-25 21:02:23 +0000

    Make possible opening links.

--- lib/app/pipelines/tab_element_widget/processors/add_markdown.dart
+++ lib/app/pipelines/tab_element_widget/processors/add_markdown.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:haskell_is_beautiful/app/entities/content_piece.dart';
 import 'package:haskell_is_beautiful/base/pipelines.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AddMarkdown extends Processor {
   @override
   Object safeExecute(PipelineContext context) {
     final piece = context.get<ContentPiece>("data");
 
-    return MarkdownBody(data: piece.data);
+    return MarkdownBody(
+        data: piece.data,
+        onTapLink: (title, url, _) {
+          canLaunch(url).then((value) {
+            if (value) launch(url);
+          });
+        });
   }
 
   @override
