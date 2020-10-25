e764fc77f85940ee03225cb902366fa1a906a26e
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-10-25 20:42:45 +0000

    Added possibility to provide markdown.

--- lib/app/pipelines/tab_element_widget/build_content_element.dart
+++ lib/app/pipelines/tab_element_widget/build_content_element.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:haskell_is_beautiful/app/pipelines/tab_element_widget/processors/add_code.dart';
 import 'package:haskell_is_beautiful/app/pipelines/tab_element_widget/processors/add_code_from_asset.dart';
+import 'package:haskell_is_beautiful/app/pipelines/tab_element_widget/processors/add_markdown.dart';
 import 'package:haskell_is_beautiful/app/pipelines/tab_element_widget/processors/add_remote_code.dart';
 import 'package:haskell_is_beautiful/app/entities.dart';
 import 'package:haskell_is_beautiful/base/pipeline.dart';
 
 class BuildContentElement extends Pipeline {
   BuildContentElement() : super([
-      AddCode(), AddCodeFromAsset(), AddRemoteCode()
+      AddCode(), AddCodeFromAsset(), AddRemoteCode(), AddMarkdown(),
     ]);
 
   Widget buildElement(ContentPiece data) {
---
+++ lib/app/pipelines/tab_element_widget/processors/add_markdown.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:haskell_is_beautiful/app/entities/content_piece.dart';
+import 'package:haskell_is_beautiful/base/pipelines.dart';
+
+class AddMarkdown extends Processor {
+  @override
+  Object safeExecute(PipelineContext context) {
+    final piece = context.get<ContentPiece>("data");
+
+    return MarkdownBody(data: piece.data);
+  }
+
+  @override
+  bool safeCondition(PipelineContext context) {
+    final piece = context.get<ContentPiece>("data");
+
+    return piece != null && piece.type == "markdown";
+  }
+}
