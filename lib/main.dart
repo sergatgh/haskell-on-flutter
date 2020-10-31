1f8ecb2c630fb6d6b1894b20ccd62da6cf09f67d
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-10-31 15:03:50 +0000

    Wrap with stateless widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'app/main_wrapper.dart';
 
-void main() => runApp(HaskellPocketBookApp());
+void main() => runApp(MainWrapper());
+
+// Main wrapper should be stateless.
+class MainWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) =>
+      MaterialApp(home: HaskellPocketBookApp());
+}
